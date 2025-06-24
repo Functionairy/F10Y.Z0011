0706afedf0f1887af4cb93d50722b78c6a0e4a0f
SURFEIT\David <david.coats@gmail.com>
2025-06-24 16:32:04 -0700

    Add path values to F10Y.Z0011.Z001.

---
+++ source/F10Y.Z0011.Z001/Code/Values/IPaths.cs
@@ -0,0 +1,16 @@
+using System;
+
+using F10Y.T0003;
+
+
+namespace F10Y.Z0011.Z001
+{
+    [ValuesMarker]
+    public partial interface IPaths
+    {
+        /// <summary>
+        /// <para><value>C:\Temp\Temp.txt</value></para>
+        /// </summary>
+        public string C_Temp_Temp_txt => @"C:\Temp\Temp.txt";
+    }
+}
---
+++ source/F10Y.Z0011.Z001/Code/ΩInstances/Paths.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace F10Y.Z0011.Z001
+{
+    public class Paths : IPaths
+    {
+        #region Infrastructure
+
+        public static IPaths Instance { get; } = new Paths();
+
+
+        private Paths()
+        {
+        }
+
+        #endregion
+    }
+}
